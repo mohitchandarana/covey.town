e1ed823624c065975e24231c6f397c7a281fca6d
mohitchandarana <chandarana.m@husky.neu.edu>
2021-04-02 11:25:39 -0400

    Added 4 more avatars and created the dropdown to choose avatars

--- frontend/src/components/Authentication/Profile.tsx
+++ frontend/src/components/Authentication/Profile.tsx
@@ -1,6 +1,44 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import {
+  Image,
+  Select
+} from '@chakra-ui/react';
+import useCoveyAppState from '../../hooks/useCoveyAppState';
+import IntroContainer from '../VideoCall/VideoFrontend/components/IntroContainer/IntroContainer';
 
+export default function Profile(): JSX.Element {
+  const { myPlayerID, players } = useCoveyAppState();
+  const myPlayer = players.find((player) => player.id === myPlayerID);
+  const [ currentAvatar, setCurrentAvatar ] = useState<string>('misa');
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setCurrentAvatar(event.target.value)
+  }
 
-export default function Profile() {
-    return <h1>Profile Page</h1>
+  return (
+    <IntroContainer>
+      <h1>Profile Page</h1>
+      <h2>Current User Avatar</h2>
+      <Image
+        key={Date.now()}
+        boxSize="100px"
+        objectFit="contain"
+        src={`${process.env.PUBLIC_URL}/assets/atlas/tuxemon-${currentAvatar}/${currentAvatar}-front.png`}
+        alt="Tuxemon Misa"
+      />
+      <h2> Select Avatar from Dropdown</h2>
+      <Select
+        value={currentAvatar}
+        onChange={handleChange} >
+        <option value="misa">Tuxemon Misa </option>
+        <option value="catgirl">Tuxemon Catgirl</option>
+        <option value="female">Tuxemon Female </option>
+        <option value="childactor">Tuxemon ChildActor</option>
+        <option value="beachcomber">Tuxemon BeachComber </option>
+      </Select>
+      
+    
+      
+
+    </IntroContainer>
+  );
 } 
