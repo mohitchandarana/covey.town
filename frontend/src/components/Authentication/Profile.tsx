bf84608c3e6442d6607ac53d7f98a4474a87bae5
mohitchandarana <chandarana.m@husky.neu.edu>
2021-04-02 13:08:23 -0400

    Added currentAvatar field to Player and polished Profile Page

--- frontend/src/components/Authentication/Profile.tsx
+++ frontend/src/components/Authentication/Profile.tsx
@@ -1,44 +1,75 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import {
+  Box,
+  Center,
+  Heading,  
   Image,
-  Select
+  Select,
+  Stack
 } from '@chakra-ui/react';
+import { BsFillInfoCircleFill } from 'react-icons/bs'
 import useCoveyAppState from '../../hooks/useCoveyAppState';
 import IntroContainer from '../VideoCall/VideoFrontend/components/IntroContainer/IntroContainer';
 
 export default function Profile(): JSX.Element {
   const { myPlayerID, players } = useCoveyAppState();
   const myPlayer = players.find((player) => player.id === myPlayerID);
-  const [ currentAvatar, setCurrentAvatar ] = useState<string>('misa');
+  const [ currentAvatar, setCurrentAvatar ] = useState<string>(myPlayer?.currentAvatar || 'misa');
+  const [ avatarPreview, setAvatarPreview ] = useState<string>('misa');
   const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setCurrentAvatar(event.target.value)
+    setAvatarPreview(event.target.value)
   }
 
   return (
     <IntroContainer>
-      <h1>Profile Page</h1>
-      <h2>Current User Avatar</h2>
-      <Image
-        key={Date.now()}
-        boxSize="100px"
-        objectFit="contain"
-        src={`${process.env.PUBLIC_URL}/assets/atlas/tuxemon-${currentAvatar}/${currentAvatar}-front.png`}
-        alt="Tuxemon Misa"
-      />
-      <h2> Select Avatar from Dropdown</h2>
-      <Select
-        value={currentAvatar}
-        onChange={handleChange} >
-        <option value="misa">Tuxemon Misa </option>
-        <option value="catgirl">Tuxemon Catgirl</option>
-        <option value="female">Tuxemon Female </option>
-        <option value="childactor">Tuxemon ChildActor</option>
-        <option value="beachcomber">Tuxemon BeachComber </option>
-      </Select>
       
-    
-      
+      <Stack>
+        <Box p="4" borderWidth="1px" borderRadius="lg">
+          <Center h="50px">
+            <Heading as="h1" size="lg">Profile Page</Heading>
+          </Center>    
+        </Box>
+        <Box p="4" borderWidth="1px" borderRadius="lg">
+          <Center>
+            <Heading as="h2" size="md">Current User Avatar</Heading>
+          </Center>   
+        
+          <Center>
+            <Image
+              boxSize="100px"
+              objectFit="contain"
+              src={`${process.env.PUBLIC_URL}/assets/atlas/tuxemon-${currentAvatar}/${currentAvatar}-front.png`}
+            />
+          </Center>
+        </Box>
+        <Box p="4" borderWidth="1px" borderRadius="lg">
+          <h2> <BsFillInfoCircleFill/>To change the current Avatar, select one from the Dropdown Menu</h2> 
+        </Box>
+        <Box p="4" borderWidth="1px" borderRadius="lg">
+          <Center>
+            <Heading as="h2" size="sm">Selection Preview</Heading>
+          </Center>   
+        
+          <Center>
+            <Image
+              key={Date.now()}
+              boxSize="100px"
+              objectFit="contain"
+              src={`${process.env.PUBLIC_URL}/assets/atlas/tuxemon-${avatarPreview}/${avatarPreview}-front.png`}
+            />
+          </Center>
+        </Box>
 
+        <Select
+          value={currentAvatar}
+          onChange={handleChange} >
+          <option value="misa">Tuxemon Misa </option>
+          <option value="catgirl">Tuxemon Catgirl</option>
+          <option value="female">Tuxemon Spooky </option>
+          <option value="childactor">Tuxemon ChildActor</option>
+          <option value="beachcomber">Tuxemon BeachComber </option>
+        </Select>
+      </Stack>
     </IntroContainer>
   );
 } 
